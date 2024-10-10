b1e23ca716a811f523d21fc08dc674765ded2251
Your Name <you@example.com>
2024-10-10 09:44:56 +0300

    Был создан класс ShellEmulator, разработано окно ввода, а также функция main() - функция запуска программы

--- Project.py
+++ Project.py
@@ -1,4 +1,44 @@
 import os
 import tarfile
+import argparse
 import tkinter as tk
-from tkinter import scrolledtext, messagebox
+from tkinter import scrolledtext
+from datetime import datetime
+
+class ShellEmulator:
+    def __init__(self, root, username, vfs):
+        self.root = root
+        self.root.title("Shell Emulator")
+
+        self.output = scrolledtext.ScrolledText(root, height=20, width=80, state=tk.DISABLED, bg="blue", fg="white")
+        self.output.pack()
+
+        self.input = tk.Entry(root, width=80)
+        self.input.pack()
+
+        self.username = username
+        self.vfs = vfs
+
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Shell Emulator")
+    parser.add_argument('--user', required=True, help="Username for shell prompt")
+    parser.add_argument('--vfs', required=True, help="Path to the virtual file system (tar archive)")
+    parser.add_argument('--script', help="Path to the startup script")
+
+    args = parser.parse_args()
+
+    vfs = args.vfs
+    root = tk.Tk()
+    shell = ShellEmulator(root, args.user, vfs)
+
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
